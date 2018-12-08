371448313594c9742382ad614d4faf264713c27d
vinayincaendo <vinay@incaendo.com>
2018-12-08 14:19:34 +0530

    ui changes

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,42 +1,66 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const bcrypt = require ("bcrypt");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const User = require('../models/user');
+const User = require("../models/user");
 
-router.post("/register", (req, res, next) => {
-   bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
+router.post("/signup", (req, res, next) => {
+    console.log("signup");
+    User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: "Mail exists"
+        });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+            console.log("vinakjhkjh");
+          if (err) {
             return res.status(500).json({
-                error: err
+              error: err
             });
-        }else{
+          } else {
             const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash,
+              _id: new mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash
             });
             user
-            .save()
-            .then(result => {
+              .save()
+              .then(result => {
                 console.log(result);
                 res.status(201).json({
-                    message: 'user created.'
+                  message: "User created"
                 });
-            })
-            .catch(err => {
+              })
+              .catch(err => {
                 console.log(err);
                 res.status(500).json({
-                    error: err
+                  error: err
                 });
-            });
-        }
+              });
+          }
+        });
+      }
     });
 });
 
-
-
-
+router.delete("/:userId", (req, res, next) => {
+  User.remove({ _id: req.params.userId })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: "User deleted"
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
 
 module.exports = router;
